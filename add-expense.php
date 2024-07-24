1163b6577a98cc4c3bb885ef75a6b7d957fda65f
razaali572 <alirazaali5723@gmail.com>
2024-07-24 14:47:16 +0500

    expness and receving changng

--- add-expense.php
+++ add-expense.php
@@ -6,12 +6,12 @@
 <main id="main" class="main">
 
     <div class="pagetitle">
-        <h1>School Profile</h1>
+        <h1>Daily expences and receiving</h1>
         <nav>
             <ol class="breadcrumb">
                 <!-- <li class="breadcrumb-item"><a href="index.html">Home</a></li>
       <li class="breadcrumb-item">Users</li> -->
-                <li class="breadcrumb-item active">School name here</li>
+                <li class="breadcrumb-item active">My School system</li>
             </ol>
         </nav>
     </div><!-- End Page Title -->
@@ -21,53 +21,40 @@
             <div class="col-md-8 offset-md-2">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Multi Columns Form</h5>
+                        <h5 class="card-title text-center ">Add Expenscss</h5>
 
                         <!-- Multi Columns Form -->
                         <form class="row g-3">
+                        <div class="col-md-12">
+                                <label for="inputCity" class="form-label">Date</label>
+                                <input type="text" class="form-control" id="inputCity">
+                            </div>
                             <div class="col-md-12">
-                                <label for="inputName5" class="form-label">Your Name</label>
+                                <label for="inputName5" class="form-label">Pay</label>
                                 <input type="text" class="form-control" id="inputName5">
                             </div>
                             <div class="col-md-6">
-                                <label for="inputEmail5" class="form-label">Email</label>
+                                <label for="inputEmail5" class="form-label">Electricity</label>
                                 <input type="email" class="form-control" id="inputEmail5">
                             </div>
                             <div class="col-md-6">
-                                <label for="inputPassword5" class="form-label">Password</label>
+                                <label for="inputPassword5" class="form-label">Rent</label>
                                 <input type="password" class="form-control" id="inputPassword5">
                             </div>
                             <div class="col-12">
-                                <label for="inputAddress5" class="form-label">Address</label>
-                                <input type="text" class="form-control" id="inputAddres5s" placeholder="1234 Main St">
+                                <label for="inputAddress5" class="form-label">Furniture</label>
+                                <input type="text" class="form-control" id="inputAddres5s" placeholder="">
                             </div>
                             <div class="col-12">
-                                <label for="inputAddress2" class="form-label">Address 2</label>
-                                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                                <label for="inputAddress2" class="form-label">construction</label>
+                                <input type="text" class="form-control" id="inputAddress2" placeholder="">
                             </div>
-                            <div class="col-md-6">
-                                <label for="inputCity" class="form-label">City</label>
+                            <div class="col-md-12">
+                                <label for="inputCity" class="form-label">Other Expencess</label>
                                 <input type="text" class="form-control" id="inputCity">
                             </div>
-                            <div class="col-md-4">
-                                <label for="inputState" class="form-label">State</label>
-                                <select id="inputState" class="form-select">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
-                                </select>
-                            </div>
-                            <div class="col-md-2">
-                                <label for="inputZip" class="form-label">Zip</label>
-                                <input type="text" class="form-control" id="inputZip">
-                            </div>
-                            <div class="col-12">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                                    <label class="form-check-label" for="gridCheck">
-                                        Check me out
-                                    </label>
-                                </div>
-                            </div>
+                           
+                          
                             <div class="text-center">
                                 <button type="submit" class="btn btn-primary">Submit</button>
                                 <button type="reset" class="btn btn-secondary">Reset</button>
--- add-receiving.php
+++ add-receiving.php
@@ -6,12 +6,12 @@
 <main id="main" class="main">
 
     <div class="pagetitle">
-        <h1>School Profile</h1>
+        <h1>Daily Receving</h1>
         <nav>
             <ol class="breadcrumb">
                 <!-- <li class="breadcrumb-item"><a href="index.html">Home</a></li>
       <li class="breadcrumb-item">Users</li> -->
-                <li class="breadcrumb-item active">School name here</li>
+                <li class="breadcrumb-item active">My school system</li>
             </ol>
         </nav>
     </div><!-- End Page Title -->
@@ -21,53 +21,35 @@
             <div class="col-md-8 offset-md-2">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Multi Columns Form</h5>
+                        <h5 class="card-title text-center">PaymentReceving Form</h5>
 
                         <!-- Multi Columns Form -->
                         <form class="row g-3">
                             <div class="col-md-12">
-                                <label for="inputName5" class="form-label">Your Name</label>
+                                <label for="inputName5" class="form-label">Date</label>
                                 <input type="text" class="form-control" id="inputName5">
                             </div>
                             <div class="col-md-6">
-                                <label for="inputEmail5" class="form-label">Email</label>
+                                <label for="inputEmail5" class="form-label">Student fee</label>
                                 <input type="email" class="form-control" id="inputEmail5">
                             </div>
                             <div class="col-md-6">
-                                <label for="inputPassword5" class="form-label">Password</label>
+                                <label for="inputPassword5" class="form-label">Student Fine</label>
                                 <input type="password" class="form-control" id="inputPassword5">
                             </div>
                             <div class="col-12">
-                                <label for="inputAddress5" class="form-label">Address</label>
-                                <input type="text" class="form-control" id="inputAddres5s" placeholder="1234 Main St">
+                                <label for="inputAddress5" class="form-label">Admination Fee</label>
+                                <input type="text" class="form-control" id="inputAddres5s" placeholder="">
                             </div>
-                            <div class="col-12">
-                                <label for="inputAddress2" class="form-label">Address 2</label>
-                                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="inputCity" class="form-label">City</label>
-                                <input type="text" class="form-control" id="inputCity">
-                            </div>
-                            <div class="col-md-4">
+                            <!-- <div class="col-md-4">
                                 <label for="inputState" class="form-label">State</label>
                                 <select id="inputState" class="form-select">
                                     <option selected>Choose...</option>
                                     <option>...</option>
                                 </select>
-                            </div>
-                            <div class="col-md-2">
-                                <label for="inputZip" class="form-label">Zip</label>
-                                <input type="text" class="form-control" id="inputZip">
-                            </div>
-                            <div class="col-12">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                                    <label class="form-check-label" for="gridCheck">
-                                        Check me out
-                                    </label>
-                                </div>
-                            </div>
+                            </div> -->
+                           
+                          
                             <div class="text-center">
                                 <button type="submit" class="btn btn-primary">Submit</button>
                                 <button type="reset" class="btn btn-secondary">Reset</button>
