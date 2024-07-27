a2717234811dab98d951e3e3924304c6113a3bdc
abdulahJavaid <cheema1140@gmail.com>
2024-07-27 12:47:20 +0500

    nothing

--- includes/init.php
+++ includes/init.php
@@ -4,9 +4,13 @@
     session_start(); // for the use fo $_SESSION super global
     ob_start(); // output buffering for redirection
 
+    // necessary files
     require_once("db_connection/configs.php"); // database connection constants
     require_once("db_connection/connection.php"); // database connection
-    require_once("parameters/class_sections.php"); // database connection
     require_once("functions.php"); // all the functions for my school system
+
+
+    // parameter files for easier management
+    require_once("parameters/class_sections.php"); // specify how many classes and sections for each class
 
 ?>
