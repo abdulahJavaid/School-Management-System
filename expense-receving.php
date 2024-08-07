2714558da16e0fe5799539a3b380e1dc833e3f84
abdulahJavaid <cheema1140@gmail.com>
2024-08-07 14:22:46 +0500

    list

--- expense-receving.php
+++ expense-receving.php
@@ -19,70 +19,74 @@
 
       <!-- First Card -->
       <!-- <div class="col-md-6"> -->
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Expense and Receiving</h5>
-            <p>Total expense and receiving records for <code>today</code>.</p>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Expense and Receiving</h5>
+          <p>Total expense and receiving records for <code>today</code>.</p>
 
-            <table class="table table-bordered border-primary tbl">
-              <thead>
-                <tr>
-                  <th scope="col">Date</th>
-                  <th scope="col">Comment</th>
-                  <th scope="col">Expense</th>
-                  <th scope="col">Receiving</th>
-                  <th scope="col">Image</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-                // the current date
-                $date = date('Y-m-d', time());
+          <table class="table table-bordered border-primary tbl">
+            <thead>
+              <tr>
+                <th scope="col">Date</th>
+                <th scope="col">Comment</th>
+                <th scope="col">Expense</th>
+                <th scope="col">Receiving</th>
+                <th scope="col">Image</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              // the current date
+              $date = date('Y-m-d', time());
 
-                // select the records from database of today
-                $query = "SELECT * FROM expense_receiving WHERE date='$date' ORDER BY er_id DESC";
-                $result = query($query);
-                while ($row = mysqli_fetch_assoc($result)) {
-                ?>
-                  <tr>
-                    <td><?php echo $row['date']; ?></td>
-                    <td><?php echo $row['comment']; ?></td>
-                    <td><?php 
-                    if($row['expense'] == 0)
-                    echo '---';
-                    else
-                    echo 'Rs. '.$row['expense']; 
-                    ?></td>
-                    <td><?php 
-                    if($row['receiving'] == 0)
-                    echo '---';
-                    else
-                    echo 'Rs. '.$row['receiving']; 
-                    ?></td>
-                    <td><img src="uploads/expense-receiving/<?php echo $row['image']; ?>" width="30px" height="30px" alt="img"></td>
-                  </tr>
-                <?php
-                }
-                ?>
+              // select the records from database of today
+              $query = "SELECT * FROM expense_receiving WHERE date='$date' ORDER BY er_id DESC";
+              $result = query($query);
+              $exp = 0;
+              $rec = 0;
+              while ($row = mysqli_fetch_assoc($result)) {
+                $exp += (int) $row['expense'];
+                $rec += (int) $row['receiving'];
+              ?>
                 <tr>
-                  <td>---</td>
-                  <td>---</td>
-                  <td><strong>Total: </strong></td>
-                  <td><strong>Total: </strong></td>
-                  <td>---</td>
+                  <td><?php echo $row['date']; ?></td>
+                  <td><?php echo $row['comment']; ?></td>
+                  <td><?php
+                      if ($row['expense'] == 0)
+                        echo '---';
+                      else
+                        echo 'Rs. ' . $row['expense'];
+                      ?></td>
+                  <td><?php
+                      if ($row['receiving'] == 0)
+                        echo '---';
+                      else
+                        echo 'Rs. ' . $row['receiving'];
+                      ?></td>
+                  <td><img src="uploads/expense-receiving/<?php echo $row['image']; ?>" width="30px" height="30px" alt="img"></td>
                 </tr>
-                <tr>
-                  <td>---</td>
-                  <td>---</td>
-                  <td colspan="2"><strong>Total: </strong></td>
-                  <!-- <td><strong>Total: </strong></td> -->
-                  <td>---</td>
-                </tr>
-              </tbody>
-              </table>
+              <?php
+              }
+              ?>
+              <tr>
+                <td>---</td>
+                <td>---</td>
+                <td><strong></strong>Rs. <?php echo $exp; ?></td>
+                <td><strong></strong>Rs. <?php echo $rec; ?></td>
+                <td>---</td>
+              </tr>
+              <tr>
+                <td>---</td>
+                <td>---</td>
+                <td colspan="2"><strong>Total: </strong>Rs. <?php echo $rec-$exp; ?></td>
+                <!-- <td><strong>Total: </strong></td> -->
+                <td>---</td>
+              </tr>
+            </tbody>
+          </table>
 
-          </div>
         </div>
+      </div>
 
 
     </div><!-- End Row -->
--- remember.php
+++ remember.php
@@ -7,6 +7,30 @@
     // 5. define access level for teacher & admins(accountant, profiler, scheduler)
     // 6. students parents are missing
     // 7. Required missing from all form fields
+?>
+
+
+<?php 
+session_start();
+ob_start();
+require_once('db_connection/configs.php');
+require_once('db_connection/connection.php');
+require_once('includes/functions.php');
+
+$date = date('Y-m-d', time());
+
+$query = "SELECT * FROM expense_receiving WHERE date='$date'";
+$result = mysqli_query($conn, $query);
+$exp = 0;
+$rec = 0;
+while ($row = mysqli_fetch_assoc($result)) {
+    $exp += (int) $row['expense'];
+    $rec += (int) $row['receiving'];
+}
+echo $exp;
+echo ' '.$rec;
+
+
 
 
 ?>
