f9779c9d8eb04d11150f20188d94b92213ba0fa7
razaali572 <alirazaali5723@gmail.com>
2024-08-13 19:00:22 +0500

    modify

--- student-migration.php
+++ student-migration.php
@@ -12,50 +12,6 @@
                 <li class="breadcrumb-item active">My School System</li>
             </ol>
         </nav>
-
-    </div><!-- End Page Title -->
-
-    <!-- class and sections -->
-    <div class="pagetitle">
-        <div class="row">
-            <?php
-            // $color = filter_input(INPUT_POST, 'color', FILTER_SANITIZE_STRING);
-
-            ?>
-            <form action="" method="post">
-                <div class="col-md-4">
-                    <select id="inputState" name="select" class="form-select">
-                        <option selected>Choose Class</option>
-                        <?php
-                        // fetching all the classes 
-                        $result = sql_select_all("all_classes");
-                        while ($row = mysqli_fetch_assoc($result)) {
-
-                        ?>
-                            <optgroup label="Class: <?php echo $row['class_name']; ?>">
-                                <?php
-                                // fetching the related sections
-                                $result1 = sql_where("class_sections", "fk_class_id", $row['class_id']);
-                                while ($row1 = mysqli_fetch_assoc($result1)) {
-                                ?>
-                                    <option value="<?php echo $row['class_id'] . " " . $row1['section_id']; ?>"><?php echo $row['class_name'] . " " . $row1['section_name']; ?></option>
-                                <?php
-                                }
-                                ?>
-                            </optgroup>
-                        <?php } ?>
-                    </select>
-                </div>
-                <div class="row">
-                    <div class="col-md-2">
-                        <input type="submit" name="submit" class="btn btn-md btn-primary button mx-auto" value="See Class Data">
-                    </div>
-                    <div class="d-flex justify-content-end">
-                        <a class="btn btn-primary button" href="./student-profile.php">Add Student</a>
-                    </div>
-                </div>
-            </form>
-        </div>
     </div><!-- End Page Title -->
 
     <section class="section profile">
@@ -74,114 +30,41 @@
                                     <th scope="col">Name</th>
                                     <th scope="col">Class</th>
                                     <th scope="col">Section</th>
-                                    <th scope="col">Monthly Fee</th>
-                                    <th scope="col">Dues</th>
-                                    <th scope="col">See Profile</th>
-                                    <th scope="col">Edit</th>
+                                    <th scope="col">Progress Date</th>
+                                    <th scope="col">View Report</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
+                              // get student info
+                              $query = "SELECT * FROM progress_report INNER JOIN ";
+                              $query .= "student_profile ON progress_report.fk_student_id=student_profile.student_id ";
+                              $query .= "INNER JOIN student_class ON student_profile.student_id=student_class.fk_student_id ";
+                              $query .= "INNER JOIN class_sections ON student_class.fk_section_id=class_sections.section_id ";
+                              $query .= "INNER JOIN all_classes ON class_sections.fk_class_id=all_classes.class_id ";
+                        $result = query($query);
+                        $rows = mysqli_fetch_assoc($result);
 
-                                if (isset($_POST['submit'])) {
-                                    // Selecting students from the specified class
-                                    $fetch = $_POST['select'];
-                                    $length = strlen($fetch);
-                                    $find = strpos($fetch, ' ');
-                                    $number = $find + 1;
-                                    $useable = $length - $number;
-                                    $useable1 = $find;
+                               
+                                // Fetch data from the database
 
-                                    $section = substr($fetch, -$useable);
-                                    $class = substr($fetch, 0, $find);
-                                    $section = (int) $section;
-                                    $class = (int) $class;
-
-                                    // to fetch all related students
-                                    // $que = "SELECT * FROM student_class WHERE fk_section_id = $section AND fk_class_id = $class";
-                                    // $pass_que = mysqli_query($conn, $que);
-                                    // $res = mysqli_fetch_assoc($pass_que);
-                                    // $sec = $res['fk_section_id'];
-                                    // $clas = $res['fk_class_id'];
-
-                                    // $que2 = "SELECT section_name FROM class_sections WHERE section_id = $section";
-                                    // $pass_que2 = mysqli_query($conn, $que2);
-                                    // $res2 = mysqli_fetch_assoc($pass_que2);
-                                    // $_section = $res2['section_name'];
-                                    // $que3 = "SELECT class_name FROM all_classes WHERE class_id = $class";
-                                    // $pass_que3 = mysqli_query($conn, $que3);
-                                    // $res3 = mysqli_fetch_assoc($pass_que3);
-                                    // $_class = $res3['class_name'];
-
-                                    $que = "SELECT * FROM all_classes ";
-                                    $que .= "INNER JOIN class_sections ON all_classes.class_id = class_sections.fk_class_id ";
-                                    $que .= "INNER JOIN student_class ON class_sections.section_id = student_class.fk_section_id ";
-                                    $que .= "INNER JOIN student_profile ON student_class.fk_student_id = student_profile.student_id ";
-                                    $que .= "WHERE class_id = '$class' AND section_id = '$section' AND status='1' AND student_status='1'";
-                                    // $que .= "student_class.fk_section_id = class_sections.section_id INNER JOIN all_classes ";
-                                    // $que .= "ON class_sections.fk_class_id = all_classes.class_id INNER JOIN student_profile ON ";
-                                    // $que .= "student_class.fk_student_id = student_profile.student_id";
-
-
-
-                                    // $table = 'student_profile';
-                                    // $val1 = $_POST['class'];
-                                    // $val2 = (string) $_POST['section'];
-                                    // $op1 = 'class';
-                                    // $op2 = 'section';
-                                    $result = mysqli_query($conn, $que);
-                                    while ($row = mysqli_fetch_assoc($result)) {
-                                        //     // to fetch student data
-                                        // $std = $row['fk_student_id'];
-                                        // $que1 = "SELECT * FROM student_profile WHERE student_id = $std";
-                                        // $pass_que1 = mysqli_query($conn, $que1);
-                                        // $res1 = mysqli_fetch_assoc($pass_que1);
-                                        // // getting the class
-                                        // $que2 = "SELECT * FROM all_classes WHERE class_id = $class";
-                                        // $pass_que2 = mysqli_query($conn, $que2);
-                                        // $res2 = mysqli_fetch_assoc($pass_que2);
-                                        // $class = $res2['class_name'];
-                                        // // getting the section
-                                        // $que3 = "SELECT * FROM class_sections WHERE section_id = $section";
-                                        // $pass_que3 = mysqli_query($conn, $que3);
-                                        // $res3 = mysqli_fetch_assoc($pass_que3);
-                                        // $section = $res3['section_name'];
-
-                                ?>
-                                        <tr>
-                                            <td><?php echo $row['roll_no']; ?></td><!--scope="row" -->
-                                            <td><?php echo $row['name']; ?></td>
-                                            <td><?php echo $row['class_name']; ?></td>
-                                            <td><?php echo $row['section_name']; ?></td>
-                                            <td>null</td>
-                                            <td><a href="view-student.php?id=<?php echo $row['student_id']; ?>">View profile</a></td>
-                                            <td><a href="edit-student.php?id=<?php echo $row['student_id']; ?>">Edit profile</a></td>
-                                        </tr>
-
-                                    <?php }
-                                } else {
-                                    // Selecting all student records from database
-                                    $queri = "SELECT * FROM student_profile INNER JOIN student_class ";
-                                    $queri .= "ON student_profile.student_id = student_class.fk_student_id INNER JOIN ";
-                                    $queri .= "class_sections ON student_class.fk_section_id = class_sections.section_id INNER JOIN ";
-                                    $queri .= "all_classes ON class_sections.fk_class_id = all_classes.class_id WHERE student_status='1' AND status='1'";
-
-                                    $result = mysqli_query($conn, $queri);
-                                    while ($row = mysqli_fetch_assoc($result)) {
-                                    ?>
-                                        <tr>
-                                            <td><?php echo $row['roll_no']; ?></td><!--scope="row" -->
-                                            <td><?php echo $row['name']; ?></td>
-                                            <td><?php echo $row['class_name']; ?></td>
-                                            <td><?php echo $row['section_name']; ?></td>
-                                            <td>Rs. <?php echo $row['fee_amount']; ?></td>
-                                            <td>null</td>
-                                            <td><a href="view-student.php?id=<?php echo $row['student_id']; ?>">View profile</a></td>
-                                            <td><a href="edit-student.php?id=<?php echo $row['student_id']; ?>">Edit profile</a></td>
-                                        </tr>
-                                <?php
+                                if ($result->num_rows > 0) {
+                                    // Output data of each row
+                                    while($row = $result->fetch_assoc()) {
+                                        echo "<tr>";
+                                        echo "<td>" . $row['roll_no'] . "</td>";
+                                        echo "<td>" . $row['name'] . "</td>";
+                                        echo "<td>" . $row['class_name'] . "</td>";
+                                        echo "<td>" . $row['section_name'] . "</td>";
+                                        echo "<td>" . $row['date'] . "</td>";
+                                        echo "<td><a href='view-progress-report.php?roll_no=" . $row['roll_no'] . "' class='btn btn-primary'>View Report</a></td>";
+                                        echo "</tr>";
                                     }
+                                } else {
+                                    echo "<tr><td colspan='6'>No students found.</td></tr>";
                                 }
+
+                                $conn->close();
                                 ?>
                             </tbody>
                         </table>
@@ -193,7 +76,3 @@
         </div>
     </section>
 
-</main><!-- End #main -->
-
-<!-- ======= Footer ======= -->
-<?php include_once("includes/footer.php"); ?>
