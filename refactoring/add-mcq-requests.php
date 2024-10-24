bb47970040acecd392e0ac90bd9b34fbc4a61893
abdulahJavaid <cheema1140@gmail.com>
2024-10-24 16:03:19 +0500

    the upload file login change

--- refactoring/add-mcq-requests.php
+++ refactoring/add-mcq-requests.php
@@ -97,8 +97,9 @@ if (isset($_POST['auto_mcqs'])) {
 
             if (strpos($line, 'O.') === 0) {
                 // Remove 'O.' and get the options
-                $options = trim(substr($line, 2)); // Remove 'O.' from the start
-                $optionList = explode(' ', $options);
+                $options = trim(substr($line, 3)); // Remove 'O.{' from the start
+                $options = trim(substr($line, 0, -1)); // Remove '}' from the end
+                $optionList = explode('} {', $options);
                 $optionCount = 1;
 
                 foreach ($optionList as $option) {
