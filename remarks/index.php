314502aa9d663e9ce0b33c93305087e5274b701f
abdulahJavaid <cheema1140@gmail.com>
2024-12-07 15:46:10 +0500

    adjustments for live domain code

--- remarks/index.php
+++ remarks/index.php
@@ -1,14 +1,11 @@
-
 <?php
+session_start();
+ob_start();
+
 // if the user is logged in
 if (!isset($_SESSION['username'])) {
   header("Location: ./login.php");
 }
-?>
-
-<?php
-session_start();
-ob_start();
 
 header("Location: ./profile.php")
 ?>
--- remarks/remark.php
+++ remarks/remark.php
@@ -1,12 +1,14 @@
 <?php
-// database connection constants
-
-use FontLib\Table\Type\head;
-
+// // database connection constants
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', '_myschool_db');
+// database connection constants
+// define('DB_SERVER', 'localhost');
+// define('DB_USER', 'u650672385_ghulam_murtaza');
+// define('DB_PASSWORD', 'ghulam_Murtaza123!@#');
+// define('DB_NAME', 'u650672385_myschool_db');
 // database connection
 $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
 if (!$conn) {
